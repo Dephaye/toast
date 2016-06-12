6bb878b701c362bc2083f5909a043828e6ccb386
高崇彬 <769728828@qq.com>
2016-06-12 13:07:02 +0800

    no message

--- HeadPortraitSelectVC.h
+++ HeadPortraitSelectVC.h
@@ -1,10 +1,4 @@
 #import <UIKit/UIKit.h>
 
 @interface HeadPortraitSelectVC : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
-<<<<<<< HEAD
-//master fix
-=======
-//master
-//hotfix
->>>>>>> hot-fix
 @end
